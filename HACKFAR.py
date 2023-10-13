2f6fc2bbed67e2b0fac75cbfafcf4630b66587e6
Elon10musk <129167817+Elon10musk@users.noreply.github.com>
2023-10-13 23:25:03 +0530

    Update HACKFAR.py

--- HACKFAR.py
+++ HACKFAR.py
@@ -1,25 +1,45 @@
-from transformers import MBartForConditionalGeneration, MBart50Tokenizer
 import streamlit as st
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, pipeline
 
-@st.cache(allow_output_mutation=True, suppress_st_warning=True)
-def download_model():
-    model_name = "facebook/mbart-large-50-many-to-many-mmt"
-    model = MBartForConditionalGeneration.from_pretrained(model_name)
-    tokenizer = MBart50Tokenizer.from_pretrained(model_name)
-    return model, tokenizer
+# Load the model and tokenizer
+model_name = "facebook/nllb-200-distilled-600M"
+model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
 
-st.title('Hindi to English Translator')
-text = st.text_area("Enter Text:", value='', height=None, max_chars=None, key=None)
-model, tokenizer = download_model()
+# Create a translation pipeline
+translator = pipeline('translation', model=model, tokenizer=tokenizer, src_lang="hin_Deva", tgt_lang='eng_Latn', max_length=400)
 
-if st.button('Translate to English'):
-    if text == '':
-        st.write('Please enter Hindi text for translation') 
-    else: 
-        model_name = "facebook/mbart-large-50-many-to-many-mmt"
-        tokenizer.src_lang = "hi_IN"
-        encoded_hindi_text = tokenizer(text, return_tensors="pt")
-        generated_tokens = model.generate(**encoded_hindi_text, forced_bos_token_id=tokenizer.lang_code_to_id["en_XX"])
-        out = tokenizer.batch_decode(generated_tokens, skip_special_tokens=True)
-        st.write('', str(out).strip('][\''))
-else: pass
+# Streamlit UI
+st.title("Language Translator")
+
+# Input text box
+input_text = st.text_area("Enter text to translate")
+
+# Language selection
+src_lang = st.selectbox("Select source language", ["Hindi", "English", "French"])  # Add more languages as needed
+tgt_lang = st.selectbox("Select target language", ["English", "Hindi", "French"])  # Add more languages as needed
+
+# Translation button
+if st.button("Translate"):
+    if input_text:
+        # Language code mapping
+        language_codes = {
+            "Hindi": "hin_Deva",
+            "English": "eng_Latn",
+            "French": "fra_Latn",
+        }
+
+        src_lang_code = language_codes[src_lang]
+        tgt_lang_code = language_codes[tgt_lang]
+
+        # Perform translation
+        translated_text = translator(input_text, src_lang=src_lang_code, tgt_lang=tgt_lang_code)[0]["translation_text"]
+        st.write("Translated Text:")
+        st.write(translated_text)
+
+# About section
+st.sidebar.title("About")
+st.sidebar.info(
+    "This is a language translation app using the Hugging Face Transformers library. "
+    "Select the source and target languages, then enter the text to translate."
+)
